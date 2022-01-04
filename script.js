790c2ad320a13d27fc69c713453f3f1a2cfdbf97
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-01-04 17:51:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const tic_tac_toe = {
+    board:['', '', '', '', '', '', '', '', ''], 
+    symbols:['X', 'O'],
+
+    container_element: null, 
+    init: function(container){
+        this.container_element = container;
+    },
+
+    draw: function(){
+
+    }
+};
